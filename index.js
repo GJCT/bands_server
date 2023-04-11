84928be63f4a131ef36b96a1a996226215fc54a1
GJCT <119271094+GJCT@users.noreply.github.com>
2023-04-11 11:31:42 -0500

    Seccion 4 BackEnd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const path = require('path');
 require('dotenv').config();
 
@@ -6,16 +6,17 @@ require('dotenv').config();
 const app = express();
 
 //NodeServer 
-const server = require('http').createServer();
+const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
-require('./sockets/socket');
+require('./sockets/socket.js');
 
+const {PORT} = process.env;
 //Path publico
 const publicPath = path.resolve( __dirname, 'public');
 app.use(express.static(publicPath));
 
-server.listen(process.env.PORT, (err) => {
+server.listen(PORT, (err) => {
     if(err) throw new Error(err);
 
-    console.log('Servidor corriendo:', process.env.PORT);
+    console.log('Servidor corriendo:', PORT);
 })
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,22 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band( 'Twenty one Pilos' ));
+bands.addBand(new Band( 'Caramelos de Cianuro' ));
+bands.addBand(new Band( 'Guns and Rose' ));
+bands.addBand(new Band( 'Hombres G' ));
+
+//console.log(bands);
 
 //Sockets message
-io.on('connnection', client=> {
+io.on('connection', client=> {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', ()=>{
         console.log('Cliente desconectado');
     });
@@ -10,4 +24,26 @@ io.on('connnection', client=> {
         console.log('mensaje', payload);
         io.emit('mensaje', {admin: 'Nuevo mensaje'});
     });
+
+    client.on('vote-bands', (payload) => {
+        bands.voteBands(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-bands', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    // client.on('emitir-mensaje', (payload)=>{
+    //     //console.log(payload);
+    //     //io.emit('emitiendo-mensaje', payload); //Socket emite a todos los clientes
+    //     client.broadcast.emit('nuevo-mensaje', payload); // emite a todos menos al cliente quien lo emite
+    // });
 });
